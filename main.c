e64bdb4987eb0c514d33ffcc8a51fd5ffa6ddc79
Marios Kasolas <81948737+marioskasolas@users.noreply.github.com>
2023-01-22 21:02:30 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Dwste arithmo :\n");
+    int x;
+    scanf("%d",&x);
+    int r,sum = 0;
+    do{
+        r = x % 10;
+        //printf("r :%d\n",r);
+        sum = sum + r;
+        x = x / 10;
+
+    }while(r != 0);
+    printf(" Sum :%d",sum);
+}
